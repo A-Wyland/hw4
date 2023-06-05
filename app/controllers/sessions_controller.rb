1243d795f96f652659d0122cc84c396e9a2e24e3
Adrian Wyland <adrian.wyland@kellogg.northwestern.edu>
2023-06-05 00:06:49 +0000

    In progress sync all

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email"=>params["email"]})
+    #user autentication / do uname & pwrd match?
+    if @user 
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Hi!"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Incorrect Username or Password"
+        redirect_to "/login"
+      end
   end
 
+  #below for logging out a user
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye! Come again :)"
+    redirect_to "/login"
   end
 end
   
